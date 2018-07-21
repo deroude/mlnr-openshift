b4a0fff52d05c518f5cbe92da8db850dfe56d131
Valentin Raduti <valentin@thedotin.ro>
2018-07-21 16:37:20 +0300

    angular app accessible

--- server/index.js
+++ server/index.js
@@ -22,9 +22,11 @@ passport.use(new passportLocal.Strategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(express.static(path.join(__dirname, 'dist/mlnr-openshift')));
 app.use('/api', rest);
-
+app.use(express.static(path.join(__dirname, '../dist/mlnr-openshift/')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/mlnr-openshift/index.html'));
+});
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 let mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
